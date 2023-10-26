2434abe0c280cd88c57fcde7c46358a2c110a03c
Kyla Claire Pusag <143685825+KylaClairePusag@users.noreply.github.com>
2023-10-26 14:35:27 +0800

    db_ba3101.sql

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2023 at 04:53 AM
+-- Generation Time: Oct 26, 2023 at 08:20 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -53,7 +53,7 @@ CREATE TABLE `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   `department_id` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbstudinfo`
@@ -80,7 +80,25 @@ CREATE TABLE `tb_admin` (
 --
 
 INSERT INTO `tb_admin` (`admin_ID`, `admin_name`, `admin_password`) VALUES
-(1, 'Claire', 'mnbb');
+(101, 'Francis Balazon', 'Francis101'),
+(102, 'Alimoren Dioneces', 'Dioneces102 '),
+(103, 'Richelle Sulit', 'Sulit103 '),
+(104, 'Ganiela Catapia', 'Ganiela104 '),
+(105, 'Lea Salonga', 'Lea105 ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_attendees`
+--
+
+CREATE TABLE `tb_attendees` (
+  `attendee_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `faculty_id` int(11) DEFAULT NULL,
+  `rso_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -98,7 +116,11 @@ CREATE TABLE `tb_department` (
 --
 
 INSERT INTO `tb_department` (`department_id`, `department_name`) VALUES
-(1, 'CICS');
+(2201, 'CICS'),
+(2202, 'CABE'),
+(2203, 'YPMAP'),
+(2204, 'CICS'),
+(2205, 'CABE');
 
 -- --------------------------------------------------------
 
@@ -119,7 +141,11 @@ CREATE TABLE `tb_event` (
 --
 
 INSERT INTO `tb_event` (`event_id`, `event_title`, `event_detail`, `event_date`, `department_id`) VALUES
-(1, 'Sport Fest', 'Students Sport fest', '2023-10-31', NULL);
+(3101, 'SportFest', 'Gather everyone', '2023-10-28', NULL),
+(3102, 'Night Fest', 'Gather Everyone', '2023-10-29', NULL),
+(3103, 'Valentines Day', 'Gather Everyone', '2024-02-14', NULL),
+(3104, 'CICS Day', 'Gather Everyone', '2023-10-31', NULL),
+(3105, 'Christmas Party', 'Gather Everyone', '2023-12-22', NULL);
 
 -- --------------------------------------------------------
 
@@ -139,7 +165,11 @@ CREATE TABLE `tb_faculty` (
 --
 
 INSERT INTO `tb_faculty` (`faculty_id`, `faculty_name`, `faculty_password`, `department_id`) VALUES
-(10, 'Hance', 'njbjb', NULL);
+(1, 'CICS', '01BSUCICS', NULL),
+(2, 'CICS', '02BSUCICS', NULL),
+(3, 'CICS', '03BSUCICS', NULL),
+(4, 'CICS', '04BSUCICS', NULL),
+(5, 'CICS', '05BSUCICS', NULL);
 
 -- --------------------------------------------------------
 
@@ -159,7 +189,11 @@ CREATE TABLE `tb_rso` (
 --
 
 INSERT INTO `tb_rso` (`rso_id`, `rso_name`, `rso_password`, `department_id`) VALUES
-(1, 'Ana', 'llpoi', NULL);
+(1101, 'CICS', '1101CICS', NULL),
+(1102, 'CABE', '1101CABE', NULL),
+(1103, 'YPMAP', '1101YPMAP', NULL),
+(1104, 'JPCS', '1101JPCS', NULL),
+(1105, 'CIT', '1105CIT', NULL);
 
 --
 -- Indexes for dumped tables
@@ -183,6 +217,16 @@ ALTER TABLE `tbstudinfo`
 --
 ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`admin_ID`);
+
+--
+-- Indexes for table `tb_attendees`
+--
+ALTER TABLE `tb_attendees`
+  ADD PRIMARY KEY (`attendee_id`),
+  ADD KEY `event_id` (`event_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `faculty_id` (`faculty_id`),
+  ADD KEY `rso_id` (`rso_id`);
 
 --
 -- Indexes for table `tb_department`
@@ -231,35 +275,56 @@ ALTER TABLE `tbstudinfo`
 -- AUTO_INCREMENT for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+
+--
+-- AUTO_INCREMENT for table `tb_attendees`
+--
+ALTER TABLE `tb_attendees`
+  MODIFY `attendee_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_department`
 --
 ALTER TABLE `tb_department`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2206;
 
 --
 -- AUTO_INCREMENT for table `tb_event`
 --
 ALTER TABLE `tb_event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3106;
 
 --
 -- AUTO_INCREMENT for table `tb_faculty`
 --
 ALTER TABLE `tb_faculty`
-  MODIFY `faculty_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `faculty_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_rso`
 --
 ALTER TABLE `tb_rso`
-  MODIFY `rso_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `rso_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1106;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tbstudinfo`
+--
+ALTER TABLE `tbstudinfo`
+  ADD CONSTRAINT `fk_department` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`);
+
+--
+-- Constraints for table `tb_attendees`
+--
+ALTER TABLE `tb_attendees`
+  ADD CONSTRAINT `tb_attendees_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `tb_event` (`event_id`),
+  ADD CONSTRAINT `tb_attendees_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `tbstudinfo` (`studid`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tb_attendees_ibfk_3` FOREIGN KEY (`faculty_id`) REFERENCES `tb_faculty` (`faculty_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tb_attendees_ibfk_4` FOREIGN KEY (`rso_id`) REFERENCES `tb_rso` (`rso_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `tb_event`
