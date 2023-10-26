887adb86ce5fafda9154984bba3fd33316c42e5a
NICKO LOUISE M. NOVICIO <143924017+nicolowis@users.noreply.github.com>
2023-10-26 09:32:26 +0800

    Add files via upload

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 23, 2023 at 08:29 AM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2023 at 11:53 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,47 +24,199 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbempinfo`
+-- Table structure for table `tbl_addproduct`
 --
 
-DROP TABLE IF EXISTS `tbempinfo`;
-CREATE TABLE IF NOT EXISTS `tbempinfo` (
-  `empid` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `department` varchar(20) NOT NULL,
-  PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tbl_addproduct` (
+  `employeeid` int(255) NOT NULL,
+  `adminid` int(255) NOT NULL,
+  `productType` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tbempinfo`
+-- Dumping data for table `tbl_addproduct`
 --
 
-INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics');
+INSERT INTO `tbl_addproduct` (`employeeid`, `adminid`, `productType`, `size`, `quantity`) VALUES
+(2132, 192, 'Polo', 'Medium', 1),
+(2133, 192, 'Pants', '32', 2),
+(2132788, 192, 'Polo', 'Small', 5),
+(2132789, 192, 'Polo', 'XXL', 3),
+(2132790, 193, 'Polo', 'Large', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbstudinfo`
+-- Table structure for table `tbl_admin`
 --
 
-DROP TABLE IF EXISTS `tbstudinfo`;
-CREATE TABLE IF NOT EXISTS `tbstudinfo` (
-  `studid` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `course` varchar(20) NOT NULL,
-  PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tbl_admin` (
+  `employeeid` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tbstudinfo`
+-- Dumping data for table `tbl_admin`
 --
 
-INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit'),
-(2, 'kent', 'clark', 'bscs');
+INSERT INTO `tbl_admin` (`employeeid`, `username`, `password`) VALUES
+(21232, 'LiaquenOdlanyer@gmail.com', '123456'),
+(2132531, 'Novicionicko@gmail.com', '123456'),
+(2132612, 'Cerezoezekiel@gmail.com', '123456'),
+(2132788, 'BacongOliver@gmail.com', '123456'),
+(2132976, 'Marasiganharvey@gmail.com', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_inventoryproductin`
+--
+
+CREATE TABLE `tbl_inventoryproductin` (
+  `emplyeeid` int(255) NOT NULL,
+  `product` varchar(255) NOT NULL,
+  `quantityReceived` int(255) NOT NULL,
+  `dateReceived` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_inventoryproductin`
+--
+
+INSERT INTO `tbl_inventoryproductin` (`emplyeeid`, `product`, `quantityReceived`, `dateReceived`) VALUES
+(2132000, 'Skirt', 200, '2023-10-23 09:37:06'),
+(2132098, 'P.E Uniform', 250, '2023-10-23 09:37:35'),
+(2132124, 'Polo', 100, '2023-10-23 09:36:12'),
+(2132624, 'Blouse', 100, '2023-10-23 09:37:06'),
+(2132651, 'Pants', 200, '2023-10-23 09:36:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_inventoryproductout`
+--
+
+CREATE TABLE `tbl_inventoryproductout` (
+  `employeeid` int(255) NOT NULL,
+  `product` varchar(255) NOT NULL,
+  `quantityReceived` int(255) NOT NULL,
+  `dateReceived` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_inventoryproductout`
+--
+
+INSERT INTO `tbl_inventoryproductout` (`employeeid`, `product`, `quantityReceived`, `dateReceived`) VALUES
+(2100527, 'Pants', 250, '2023-10-23 09:39:36'),
+(2107112, 'Blouse', 80, '2023-10-23 09:39:55'),
+(2124512, 'Polo', 150, '2023-10-23 09:38:59'),
+(2156313, 'Skirt', 150, '2023-10-23 09:38:59'),
+(2165782, 'P.E Uniform', 50, '2023-10-23 09:39:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `designation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`username`, `password`, `designation`) VALUES
+('BacongOli', '123456', 'Officer'),
+('LiaquenOd', '123456', 'Inspector'),
+('MarasiganHarvey', '123456', 'Officer'),
+('Novicionicko', '123456', 'Manager'),
+('CarilloKeyo', '123456', 'Analyst');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `productid` int(255) NOT NULL,
+  `adminid` int(255) NOT NULL,
+  `productName` varchar(255) NOT NULL,
+  `productDescription` varchar(255) NOT NULL,
+  `productType` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`productid`, `adminid`, `productName`, `productDescription`, `productType`, `size`) VALUES
+(2154782, 192, 'School Uniform', 'Durable Quality', 'P.E Uniform', 'Small'),
+(2163672, 193, 'School Uniform', 'Durable Quality', 'Pants', 'XXL'),
+(2192745, 192, 'School Uniform', 'Durable Quality', 'Skirt', 'Large'),
+(2197241, 193, 'School Uniform', 'Durable Quality', 'Blouse', 'Medium'),
+(2197864, 199, 'School Uniform', 'Durable Quality', 'Polo - Shirt', 'Small');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_removeofficer`
+--
+
+CREATE TABLE `tbl_removeofficer` (
+  `employeeid` int(255) NOT NULL,
+  `adminid` int(255) NOT NULL,
+  `designation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_addproduct`
+--
+ALTER TABLE `tbl_addproduct`
+  ADD PRIMARY KEY (`employeeid`);
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`employeeid`);
+
+--
+-- Indexes for table `tbl_inventoryproductin`
+--
+ALTER TABLE `tbl_inventoryproductin`
+  ADD PRIMARY KEY (`emplyeeid`);
+
+--
+-- Indexes for table `tbl_inventoryproductout`
+--
+ALTER TABLE `tbl_inventoryproductout`
+  ADD PRIMARY KEY (`employeeid`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`productid`);
+
+--
+-- Indexes for table `tbl_removeofficer`
+--
+ALTER TABLE `tbl_removeofficer`
+  ADD PRIMARY KEY (`employeeid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
