d65a6cd3026bc80bb19a8d952c3e76b73b6ef561
Andrei Purio <144575219+DeyPurio@users.noreply.github.com>
2023-11-06 14:43:22 +0800

    Add files via upload

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 26, 2023 at 02:15 AM
+-- Generation Time: Nov 06, 2023 at 06:34 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -24,77 +24,74 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblapplicantlogin`
+-- Table structure for table `tblapplicant`
 --
 
-DROP TABLE IF EXISTS `tblapplicantlogin`;
-CREATE TABLE IF NOT EXISTS `tblapplicantlogin` (
-  `New Applicant ID` int NOT NULL,
-  `Username` varchar(25) NOT NULL,
-  `Password` varchar(25) NOT NULL
+DROP TABLE IF EXISTS `tblapplicant`;
+CREATE TABLE IF NOT EXISTS `tblapplicant` (
+  `applicationID` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tblapplicantlogin`
+-- Dumping data for table `tblapplicant`
 --
 
-INSERT INTO `tblapplicantlogin` (`New Applicant ID`, `Username`, `Password`) VALUES
-(1, 'dey', 'drei'),
-(2, 'mark', 'makmak'),
-(3, 'gez', 'lasin'),
-(4, 'jake', 'rain'),
-(5, 'seth', 'aldous');
+INSERT INTO `tblapplicant` (`applicationID`, `username`, `password`) VALUES
+('101', 'dey', 'drei'),
+('102', 'chi', 'che'),
+('103', 'gez', 'lasin'),
+('104', 'mark', 'makmak'),
+('105', 'jake', 'rain');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblapplicationform`
+-- Table structure for table `tblapplication`
 --
 
-DROP TABLE IF EXISTS `tblapplicationform`;
-CREATE TABLE IF NOT EXISTS `tblapplicationform` (
-  `ApplicationID` int NOT NULL,
-  `First Name` varchar(20) NOT NULL,
-  `Middle Name` varchar(20) NOT NULL,
-  `Last Name` varchar(20) NOT NULL,
-  `Email` varchar(20) NOT NULL,
-  `Number` varchar(20) NOT NULL,
-  `Job Position` varchar(20) NOT NULL
+DROP TABLE IF EXISTS `tblapplication`;
+CREATE TABLE IF NOT EXISTS `tblapplication` (
+  `ApplicationID` varchar(25) NOT NULL,
+  `jobID` varchar(25) NOT NULL,
+  `status` varchar(25) NOT NULL,
+  `resume` varchar(25) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tblapplicationform`
+-- Dumping data for table `tblapplication`
 --
 
-INSERT INTO `tblapplicationform` (`ApplicationID`, `First Name`, `Middle Name`, `Last Name`, `Email`, `Number`, `Job Position`) VALUES
-(1, 'John', 'D', 'Doey', '222@gmail.com', '09485784652', 'Maintenance'),
-(2, 'Seth', 'D', 'Dog', '111@gmail.com', '09824563871', 'Professor'),
-(3, 'Russel', 'V', 'Wayne', '333@gmail.com', '09874626451', 'Maintenance'),
-(4, 'Michael', 'S', 'Luffy', '444@gmail.com', '09837465836', 'Staff'),
-(5, 'Chi', 'P', 'Liz', '555@gmail.com', '09658327562', 'Professor');
+INSERT INTO `tblapplication` (`ApplicationID`, `jobID`, `status`, `resume`) VALUES
+('101', '1', 'pending', 'submitted'),
+('102', '2', 'pending', 'submitted'),
+('103', '3', 'pending', 'submitted'),
+('104', '1', 'pending ', 'submitted'),
+('105', '2', 'pending', 'submitted');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbldept`
+-- Table structure for table `tbldepartment`
 --
 
-DROP TABLE IF EXISTS `tbldept`;
-CREATE TABLE IF NOT EXISTS `tbldept` (
-  `Job Position` varchar(25) NOT NULL,
-  `Department Name` varchar(25) NOT NULL
+DROP TABLE IF EXISTS `tbldepartment`;
+CREATE TABLE IF NOT EXISTS `tbldepartment` (
+  `departmentID` varchar(25) NOT NULL,
+  `departmentName` varchar(25) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tbldept`
+-- Dumping data for table `tbldepartment`
 --
 
-INSERT INTO `tbldept` (`Job Position`, `Department Name`) VALUES
-('Maintenance', 'CICS'),
-('Professor', 'CIT'),
-('Staff', 'CAS'),
-('Professor', 'CABE'),
-('Staff', 'CICS');
+INSERT INTO `tbldepartment` (`departmentID`, `departmentName`) VALUES
+('BSU01', 'CICS'),
+('BSU02', 'CAS'),
+('BSU03', 'CABE'),
+('BSU04', 'CIT'),
+('BSU01', 'CICS');
 
 -- --------------------------------------------------------
 
@@ -104,19 +101,48 @@ INSERT INTO `tbldept` (`Job Position`, `Department Name`) VALUES
 
 DROP TABLE IF EXISTS `tblhr`;
 CREATE TABLE IF NOT EXISTS `tblhr` (
-  `ApplicationID` int NOT NULL
+  `adminID` varchar(25) NOT NULL,
+  `adminName` varchar(25) NOT NULL,
+  `applicationID` varchar(25) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tblhr`
 --
 
-INSERT INTO `tblhr` (`ApplicationID`) VALUES
-(1),
-(2),
-(3),
-(4),
-(5);
+INSERT INTO `tblhr` (`adminID`, `adminName`, `applicationID`) VALUES
+('HR1001', 'John', '101'),
+('HR1002', 'Doey', '102'),
+('HR1003', 'Mike', '103'),
+('HR1004', 'Thomas', '104'),
+('HR1005', 'Ruby', '105');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbljob`
+--
+
+DROP TABLE IF EXISTS `tbljob`;
+CREATE TABLE IF NOT EXISTS `tbljob` (
+  `jobID` varchar(25) NOT NULL,
+  `jobType` varchar(25) NOT NULL,
+  `description` varchar(25) NOT NULL,
+  `requirement` varchar(25) NOT NULL,
+  `salary` varchar(25) NOT NULL,
+  `departmentID` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbljob`
+--
+
+INSERT INTO `tbljob` (`jobID`, `jobType`, `description`, `requirement`, `salary`, `departmentID`) VALUES
+('1', 'Professor', 'Teaching college students', 'Work experience and educa', '15000', 'BSU01'),
+('2', 'Maintenance', 'To maintain the cleanline', 'Work experience and educa', '5000', 'BSU02'),
+('3', 'Security', 'Responsible for protectin', 'Work experience and educa', '7500', 'BSU05'),
+('2', 'Professor', 'Teaching college students', 'Work experience and educa', '15000', 'BSU04'),
+('1', 'Professor', 'Teaching college students', 'Work experience and educa', '15000', 'BSU03');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
