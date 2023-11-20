bd77a3d5b45a5a50471c1e6dae94f00477e94df0
John Ronald Lopez <21-39325@g.batstate-u.edu.ph>
2023-11-20 16:35:15 +0800

    final database

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 06, 2023 at 01:38 AM
+-- Generation Time: Nov 20, 2023 at 05:58 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -31,19 +31,21 @@ DROP TABLE IF EXISTS `tbadmin`;
 CREATE TABLE IF NOT EXISTS `tbadmin` (
   `adminno` varchar(50) NOT NULL,
   `adminpass` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`adminno`)
+  `empid` int DEFAULT NULL,
+  PRIMARY KEY (`adminno`),
+  KEY `fk_tbadmin_empid` (`empid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbadmin`
 --
 
-INSERT INTO `tbadmin` (`adminno`, `adminpass`) VALUES
-('1', 'adminpass1'),
-('2', 'adminpass2'),
-('3', 'adminpass3'),
-('4', 'adminpass4'),
-('5', 'adminpass5');
+INSERT INTO `tbadmin` (`adminno`, `adminpass`, `empid`) VALUES
+('1', 'adminpass1', 1),
+('2', 'adminpass2', 2),
+('3', 'adminpass3', 3),
+('4', 'adminpass4', 4),
+('5', 'adminpass5', 5);
 
 -- --------------------------------------------------------
 
@@ -55,19 +57,43 @@ DROP TABLE IF EXISTS `tbcoun`;
 CREATE TABLE IF NOT EXISTS `tbcoun` (
   `counno` varchar(50) NOT NULL,
   `counpass` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`counno`)
+  `empid` int DEFAULT NULL,
+  PRIMARY KEY (`counno`),
+  KEY `fk_tbcoun_empid` (`empid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbcoun`
 --
 
-INSERT INTO `tbcoun` (`counno`, `counpass`) VALUES
-('1', 'counselorpass1'),
-('2', 'counselorpass2'),
-('3', 'counselorpass3'),
-('4', 'counselorpass4'),
-('5', 'counselorpass5');
+INSERT INTO `tbcoun` (`counno`, `counpass`, `empid`) VALUES
+('1', 'counselorpass1', 1),
+('2', 'counselorpass2', 2),
+('3', 'counselorpass3', 3),
+('4', 'counselorpass4', 4),
+('5', 'counselorpass5', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbempinfo`
+--
+
+DROP TABLE IF EXISTS `tbempinfo`;
+CREATE TABLE IF NOT EXISTS `tbempinfo` (
+  `empid` int NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  PRIMARY KEY (`empid`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbempinfo`
+--
+
+INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
+(1, 'aguila', 'nina', 'cics');
 
 -- --------------------------------------------------------
 
@@ -77,24 +103,17 @@ INSERT INTO `tbcoun` (`counno`, `counpass`) VALUES
 
 DROP TABLE IF EXISTS `tbreports`;
 CREATE TABLE IF NOT EXISTS `tbreports` (
-  `reportno` int NOT NULL,
+  `reportno` int NOT NULL AUTO_INCREMENT,
   `reporttype` varchar(255) DEFAULT NULL,
   `report` text,
   `studno` varchar(50) DEFAULT NULL,
+  `status` varchar(200) DEFAULT NULL,
+  `remarks` varchar(200) DEFAULT NULL,
+  `datefiled` datetime DEFAULT NULL,
+  `dateresolved` datetime DEFAULT NULL,
   PRIMARY KEY (`reportno`),
   KEY `studno` (`studno`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbreports`
---
-
-INSERT INTO `tbreports` (`reportno`, `reporttype`, `report`, `studno`) VALUES
-(101, 'Academic Grievance', 'I have a concern about the grading in my History course.', '1'),
-(102, 'Campus Facilities Grievance', 'There is a problem with the heating in my dormitory room.', '2'),
-(103, 'Discrimination Grievance', 'I believe I have experienced discrimination in the workplace.', '3'),
-(104, 'Housing Grievance', 'I would like to request a room change due to noisy neighbors.', '4'),
-(105, 'Financial Aid Grievance', 'I am experiencing issues with my financial aid application.', '5');
 
 -- --------------------------------------------------------
 
@@ -106,46 +125,47 @@ DROP TABLE IF EXISTS `tbstud`;
 CREATE TABLE IF NOT EXISTS `tbstud` (
   `studno` varchar(50) NOT NULL,
   `studpass` varchar(255) NOT NULL,
-  PRIMARY KEY (`studno`)
+  `studid` int DEFAULT NULL,
+  PRIMARY KEY (`studno`),
+  KEY `fk_tbstud_studid` (`studid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbstud`
 --
 
-INSERT INTO `tbstud` (`studno`, `studpass`) VALUES
-('1', 'password1'),
-('2', 'password2'),
-('3', 'password3'),
-('4', 'password4'),
-('5', 'password5');
+INSERT INTO `tbstud` (`studno`, `studpass`, `studid`) VALUES
+('21-34780', 'bonjlaygo', 2134780),
+('21-39414', 'ivanrubion', 2139414),
+('21-30073', 'belletrayfalgar', 2130073),
+('21-39325', 'johnronaldlopez', 2139325),
+('21-31054', 'sethtada', 2131054);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbtracker`
+-- Table structure for table `tbstudinfo`
 --
 
-DROP TABLE IF EXISTS `tbtracker`;
-CREATE TABLE IF NOT EXISTS `tbtracker` (
-  `status` varchar(255) DEFAULT NULL,
-  `reportno` int DEFAULT NULL,
-  `studno` varchar(50) DEFAULT NULL,
-  `counno` varchar(50) DEFAULT NULL,
-  KEY `reportno` (`reportno`),
-  KEY `studno` (`studno`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `tbstudinfo`;
+CREATE TABLE IF NOT EXISTS `tbstudinfo` (
+  `studid` int NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL,
+  PRIMARY KEY (`studid`)
+) ENGINE=MyISAM AUTO_INCREMENT=2139415 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tbtracker`
+-- Dumping data for table `tbstudinfo`
 --
 
-INSERT INTO `tbtracker` (`status`, `reportno`, `studno`, `counno`) VALUES
-('1', 301, '1', '1'),
-('2', 302, '2', '2'),
-('3', 303, '3', '3'),
-('1', 304, '4', '4'),
-('2', 305, '5', '5');
+INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
+(2139325, 'Lopez', 'John Ronald', 'BSIT-BA-3101'),
+(2130073, 'Trayfalgar', 'Belle Colleen', 'BSIT-BA-3101'),
+(2131054, 'Tada', 'Seth Aldous', 'BSIT-BA-3101'),
+(2139414, 'Rubion', 'Ivan Gabriel', 'BSIT-BA-3101'),
+(2134780, 'Laygo', 'Bonjohn Benedict', 'BSIT-BA-3101');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
