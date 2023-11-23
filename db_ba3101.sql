6f520bc37778f91254803d8369b9f88b08bc3f70
John Ronald Lopez <21-39325@g.batstate-u.edu.ph>
2023-11-23 10:28:22 +0800

    removed auto increment function

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 20, 2023 at 05:58 AM
+-- Generation Time: Nov 23, 2023 at 02:28 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -31,7 +31,7 @@ DROP TABLE IF EXISTS `tbadmin`;
 CREATE TABLE IF NOT EXISTS `tbadmin` (
   `adminno` varchar(50) NOT NULL,
   `adminpass` varchar(255) DEFAULT NULL,
-  `empid` int DEFAULT NULL,
+  `empid` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`adminno`),
   KEY `fk_tbadmin_empid` (`empid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -41,11 +41,11 @@ CREATE TABLE IF NOT EXISTS `tbadmin` (
 --
 
 INSERT INTO `tbadmin` (`adminno`, `adminpass`, `empid`) VALUES
-('1', 'adminpass1', 1),
-('2', 'adminpass2', 2),
-('3', 'adminpass3', 3),
-('4', 'adminpass4', 4),
-('5', 'adminpass5', 5);
+('1', 'adminpass1', '1'),
+('2', 'adminpass2', '2'),
+('3', 'adminpass3', '3'),
+('4', 'adminpass4', '4'),
+('5', 'adminpass5', '5');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ DROP TABLE IF EXISTS `tbcoun`;
 CREATE TABLE IF NOT EXISTS `tbcoun` (
   `counno` varchar(50) NOT NULL,
   `counpass` varchar(255) DEFAULT NULL,
-  `empid` int DEFAULT NULL,
+  `empid` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`counno`),
   KEY `fk_tbcoun_empid` (`empid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -67,11 +67,11 @@ CREATE TABLE IF NOT EXISTS `tbcoun` (
 --
 
 INSERT INTO `tbcoun` (`counno`, `counpass`, `empid`) VALUES
-('1', 'counselorpass1', 1),
-('2', 'counselorpass2', 2),
-('3', 'counselorpass3', 3),
-('4', 'counselorpass4', 4),
-('5', 'counselorpass5', 5);
+('1', 'counselorpass1', '1'),
+('2', 'counselorpass2', '2'),
+('3', 'counselorpass3', '3'),
+('4', 'counselorpass4', '4'),
+('5', 'counselorpass5', '5');
 
 -- --------------------------------------------------------
 
@@ -81,7 +81,7 @@ INSERT INTO `tbcoun` (`counno`, `counpass`, `empid`) VALUES
 
 DROP TABLE IF EXISTS `tbempinfo`;
 CREATE TABLE IF NOT EXISTS `tbempinfo` (
-  `empid` int NOT NULL AUTO_INCREMENT,
+  `empid` varchar(50) NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
@@ -93,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
 --
 
 INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics');
+('1', 'aguila', 'nina', 'cics');
 
 -- --------------------------------------------------------
 
@@ -113,7 +113,19 @@ CREATE TABLE IF NOT EXISTS `tbreports` (
   `dateresolved` datetime DEFAULT NULL,
   PRIMARY KEY (`reportno`),
   KEY `studno` (`studno`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbreports`
+--
+
+INSERT INTO `tbreports` (`reportno`, `reporttype`, `report`, `studno`, `status`, `remarks`, `datefiled`, `dateresolved`) VALUES
+(1, 'Bullying', 'I was bullied by a blockmate.', '21-39325', 'Pending', NULL, '2023-11-22 20:45:44', NULL),
+(2, 'Academic Appeal', 'I received a failing grade in my class, is there something I can do about it?', '21-39325', 'Pending', NULL, '2023-11-22 20:46:10', NULL),
+(3, 'Harassment & Discrimination', 'I was harassed.', '21-39325', 'Pending', NULL, '2023-11-22 20:46:44', NULL),
+(4, 'Course Availability & Scheduling', 'No professor in 1 subject.', '21-39325', 'Resolved', 'Done.', '2023-11-22 20:46:55', '2023-11-22 20:49:14'),
+(5, 'Harassment & Discrimination', 'I was harassed.', '21-39325', 'Pending', NULL, '2023-11-22 20:47:08', NULL),
+(6, 'Course Availability & Scheduling', 'No professor in 1 subject.', '21-30073', 'In Progress', 'Will look into this, thanks.', '2023-11-22 20:48:14', NULL);
 
 -- --------------------------------------------------------
 
@@ -125,7 +137,7 @@ DROP TABLE IF EXISTS `tbstud`;
 CREATE TABLE IF NOT EXISTS `tbstud` (
   `studno` varchar(50) NOT NULL,
   `studpass` varchar(255) NOT NULL,
-  `studid` int DEFAULT NULL,
+  `studid` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`studno`),
   KEY `fk_tbstud_studid` (`studid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -135,11 +147,11 @@ CREATE TABLE IF NOT EXISTS `tbstud` (
 --
 
 INSERT INTO `tbstud` (`studno`, `studpass`, `studid`) VALUES
-('21-34780', 'bonjlaygo', 2134780),
-('21-39414', 'ivanrubion', 2139414),
-('21-30073', 'belletrayfalgar', 2130073),
-('21-39325', 'johnronaldlopez', 2139325),
-('21-31054', 'sethtada', 2131054);
+('21-34780', 'bonjlaygo', '21-34780'),
+('21-39414', 'ivanrubion', '21-39414'),
+('21-30073', 'belletrayfalgar', '21-30073'),
+('21-39325', 'johnronaldlopez', '21-39325'),
+('21-31054', 'sethtada', '21-31054');
 
 -- --------------------------------------------------------
 
@@ -149,7 +161,7 @@ INSERT INTO `tbstud` (`studno`, `studpass`, `studid`) VALUES
 
 DROP TABLE IF EXISTS `tbstudinfo`;
 CREATE TABLE IF NOT EXISTS `tbstudinfo` (
-  `studid` int NOT NULL AUTO_INCREMENT,
+  `studid` varchar(50) NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
@@ -161,11 +173,11 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
 --
 
 INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(2139325, 'Lopez', 'John Ronald', 'BSIT-BA-3101'),
-(2130073, 'Trayfalgar', 'Belle Colleen', 'BSIT-BA-3101'),
-(2131054, 'Tada', 'Seth Aldous', 'BSIT-BA-3101'),
-(2139414, 'Rubion', 'Ivan Gabriel', 'BSIT-BA-3101'),
-(2134780, 'Laygo', 'Bonjohn Benedict', 'BSIT-BA-3101');
+('21-39325', 'Lopez', 'John Ronald', 'BSIT-BA-3101'),
+('21-30073', 'Trayfalgar', 'Belle Colleen', 'BSIT-BA-3101'),
+('21-31054', 'Tada', 'Seth Aldous', 'BSIT-BA-3101'),
+('21-39414', 'Rubion', 'Ivan Gabriel', 'BSIT-BA-3101'),
+('21-34780', 'Laygo', 'Bonjohn Benedict', 'BSIT-BA-3101');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
