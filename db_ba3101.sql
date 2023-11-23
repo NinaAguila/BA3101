132785d39b288da616155e4c2c8704b40d9aee0a
Joshua <143677379+super-smile@users.noreply.github.com>
2023-11-23 10:14:32 +0800

    Add files via upload

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 20, 2023 at 09:44 AM
+-- Generation Time: Nov 23, 2023 at 02:13 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -29,19 +29,12 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `tbempinfo`;
 CREATE TABLE IF NOT EXISTS `tbempinfo` (
-  `empid` int NOT NULL AUTO_INCREMENT,
+  `empid` int NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbempinfo`
---
-
-INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -52,14 +45,14 @@ INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 DROP TABLE IF EXISTS `tbl_account`;
 CREATE TABLE IF NOT EXISTS `tbl_account` (
   `userID` int NOT NULL AUTO_INCREMENT,
-  `userImg` mediumblob NOT NULL,
+  `userImg` longblob NOT NULL,
   `userName` varchar(255) NOT NULL,
   `userDept` varchar(255) NOT NULL,
   `userEmail` varchar(255) NOT NULL,
   `userPass` varchar(255) NOT NULL,
   `userType` varchar(255) NOT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -70,14 +63,14 @@ CREATE TABLE IF NOT EXISTS `tbl_account` (
 DROP TABLE IF EXISTS `tbl_office`;
 CREATE TABLE IF NOT EXISTS `tbl_office` (
   `officeAccID` int NOT NULL AUTO_INCREMENT,
-  `officeImg` blob NOT NULL,
-  `officeName` varchar(255) NOT NULL,
+  `officeImg` longblob NOT NULL,
+  `designation` varchar(255) NOT NULL,
   `officeEmail` varchar(255) NOT NULL,
   `officePass` varchar(255) NOT NULL,
   `employeeID` int NOT NULL,
   PRIMARY KEY (`officeAccID`),
   KEY `empID(FK)` (`employeeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -97,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `tbl_reqhistory` (
   KEY `userID_FK(tbl_reqH)` (`orgID`),
   KEY `reqID_FK(tbl_reqH)` (`reqID`),
   KEY `officeID(tbl_reqH)` (`officeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -113,10 +106,10 @@ CREATE TABLE IF NOT EXISTS `tbl_requests` (
   `reqEventDate` date NOT NULL,
   `reqDeadline` date NOT NULL,
   `userID` int NOT NULL,
-  `currentOffice` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1',
+  `currentOffice` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Program Chair',
   PRIMARY KEY (`reqID`),
   KEY `userID(tbl_req)` (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -126,20 +119,12 @@ CREATE TABLE IF NOT EXISTS `tbl_requests` (
 
 DROP TABLE IF EXISTS `tbstudinfo`;
 CREATE TABLE IF NOT EXISTS `tbstudinfo` (
-  `studid` int NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbstudinfo`
---
-
-INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit'),
-(2, 'kent', 'clark', 'bscs');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Constraints for dumped tables
@@ -155,7 +140,7 @@ ALTER TABLE `tbl_office`
 -- Constraints for table `tbl_reqhistory`
 --
 ALTER TABLE `tbl_reqhistory`
-  ADD CONSTRAINT `officeID(tbl_reqH)` FOREIGN KEY (`officeID`) REFERENCES `tbl_account` (`userID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `officeID(tbl_reqH)` FOREIGN KEY (`officeID`) REFERENCES `tbl_office` (`officeAccID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `reqID_FK(tbl_reqH)` FOREIGN KEY (`reqID`) REFERENCES `tbl_requests` (`reqID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `userID_FK(tbl_reqH)` FOREIGN KEY (`orgID`) REFERENCES `tbl_account` (`userID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
