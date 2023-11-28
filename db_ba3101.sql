208eef5b433c767c17e6b23193cf967ab704df5d
Andrei Purio <144575219+DeyPurio@users.noreply.github.com>
2023-11-28 23:58:17 +0800

    Update db_ba3101.sql

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 23, 2023 at 02:31 AM
+-- Generation Time: Nov 28, 2023 at 03:54 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_ba3101`
+-- Database: `db_3101`
 --
 
 -- --------------------------------------------------------
@@ -30,18 +30,13 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `adminID` int NOT NULL AUTO_INCREMENT,
+  `empid` int DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`adminID`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`adminID`, `email`, `password`) VALUES
-(1, 'dreipurio@gmail.com', 'dey');
+  UNIQUE KEY `email` (`email`),
+  KEY `empid` (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -57,15 +52,7 @@ CREATE TABLE IF NOT EXISTS `applicant` (
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`applicantID`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `applicant`
---
-
-INSERT INTO `applicant` (`applicantID`, `email`, `password`, `name`) VALUES
-(1, 'deynft@gmail.com', 'dey', 'drei'),
-(2, 'mark@gmail.com', 'mark', 'makmak');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -76,28 +63,20 @@ INSERT INTO `applicant` (`applicantID`, `email`, `password`, `name`) VALUES
 DROP TABLE IF EXISTS `application`;
 CREATE TABLE IF NOT EXISTS `application` (
   `applicationID` int NOT NULL AUTO_INCREMENT,
-  `jobID` int DEFAULT NULL,
-  `applicantID` int DEFAULT NULL,
+  `jobID` int NOT NULL,
+  `applicantID` int NOT NULL,
+  `hrID` int NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `contact` varchar(20) DEFAULT NULL,
-  `resume` text,
+  `resume` varchar(255) DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`applicationID`),
   KEY `jobID` (`jobID`),
-  KEY `applicantID` (`applicantID`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `application`
---
-
-INSERT INTO `application` (`applicationID`, `jobID`, `applicantID`, `name`, `address`, `email`, `contact`, `resume`, `status`) VALUES
-(1, NULL, NULL, 'John Doey', 'Lipa City', 'doey@gmail.com', '09378352157', 'bsu.png', 'Pending'),
-(2, NULL, NULL, 'drei', 'Lipa City', 'deynft@gmail.com', '09535364742', 'infographics.png', 'Pending'),
-(3, NULL, NULL, 'drei', 'Lipa City', 'deynft@gmail.com', '09535364742', 'infographics.png', 'Pending'),
-(4, NULL, NULL, 'drei', 'Lipa City', 'deynft@gmail.com', '09535364742', 'infographics.png', 'Pending');
+  KEY `applicantID` (`applicantID`),
+  KEY `hrID` (`hrID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -108,19 +87,14 @@ INSERT INTO `application` (`applicationID`, `jobID`, `applicantID`, `name`, `add
 DROP TABLE IF EXISTS `hr`;
 CREATE TABLE IF NOT EXISTS `hr` (
   `HRID` int NOT NULL AUTO_INCREMENT,
+  `empid` int DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`HRID`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `hr`
---
-
-INSERT INTO `hr` (`HRID`, `email`, `password`, `name`) VALUES
-(1, 'dreipurio@gmail.com', 'dey', 'drei');
+  UNIQUE KEY `email` (`email`),
+  KEY `empid` (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -131,20 +105,15 @@ INSERT INTO `hr` (`HRID`, `email`, `password`, `name`) VALUES
 DROP TABLE IF EXISTS `jobpost`;
 CREATE TABLE IF NOT EXISTS `jobpost` (
   `jobID` int NOT NULL AUTO_INCREMENT,
+  `adminID` int NOT NULL,
   `jobType` varchar(100) DEFAULT NULL,
   `department` varchar(100) DEFAULT NULL,
-  `description` text,
-  `requirement` text,
+  `description` varchar(255) DEFAULT NULL,
+  `requirement` varchar(255) DEFAULT NULL,
   `salary` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`jobID`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `jobpost`
---
-
-INSERT INTO `jobpost` (`jobID`, `jobType`, `department`, `description`, `requirement`, `salary`) VALUES
-(6, 'Professor', 'CICS', 'asdasd', 'asdasd', '213122.00');
+  PRIMARY KEY (`jobID`),
+  KEY `adminID` (`adminID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -159,14 +128,7 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbempinfo`
---
-
-INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -181,15 +143,37 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tbstudinfo`
+-- Constraints for dumped tables
 --
 
-INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit'),
-(2, 'kent', 'clark', 'bscs');
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`empid`) REFERENCES `tbempinfo` (`empid`);
+
+--
+-- Constraints for table `application`
+--
+ALTER TABLE `application`
+  ADD CONSTRAINT `application_ibfk_1` FOREIGN KEY (`jobID`) REFERENCES `jobpost` (`jobID`),
+  ADD CONSTRAINT `application_ibfk_2` FOREIGN KEY (`applicantID`) REFERENCES `applicant` (`applicantID`),
+  ADD CONSTRAINT `application_ibfk_3` FOREIGN KEY (`hrID`) REFERENCES `hr` (`HRID`);
+
+--
+-- Constraints for table `hr`
+--
+ALTER TABLE `hr`
+  ADD CONSTRAINT `hr_ibfk_1` FOREIGN KEY (`empid`) REFERENCES `tbempinfo` (`empid`);
+
+--
+-- Constraints for table `jobpost`
+--
+ALTER TABLE `jobpost`
+  ADD CONSTRAINT `jobpost_ibfk_1` FOREIGN KEY (`adminID`) REFERENCES `admin` (`adminID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
