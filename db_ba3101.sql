ee393e4f5a982b6fe2fa01dd24097b8310f51d27
NICKO LOUISE M. NOVICIO <143924017+nicolowis@users.noreply.github.com>
2023-11-29 23:04:10 +0800

    Add files via upload

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 29, 2023 at 02:59 PM
+-- Generation Time: Nov 20, 2023 at 08:34 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -24,58 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbadmin`
---
-
-DROP TABLE IF EXISTS `tbadmin`;
-CREATE TABLE IF NOT EXISTS `tbadmin` (
-  `adminno` varchar(50) NOT NULL,
-  `adminpass` varchar(255) DEFAULT NULL,
-  `empid` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`adminno`),
-  KEY `fk_tbadmin_empid` (`empid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbadmin`
---
-
-INSERT INTO `tbadmin` (`adminno`, `adminpass`, `empid`) VALUES
-('1', 'adminpass1', '1'),
-('2', 'adminpass2', '2'),
-('3', 'adminpass3', '3'),
-('4', 'adminpass4', '4'),
-('5', 'adminpass5', '5');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbcoun`
---
-
-DROP TABLE IF EXISTS `tbcoun`;
-CREATE TABLE IF NOT EXISTS `tbcoun` (
-  `counno` varchar(50) NOT NULL,
-  `counpass` varchar(255) DEFAULT NULL,
-  `empid` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`counno`),
-  KEY `fk_tbcoun_empid` (`empid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbcoun`
---
-
-INSERT INTO `tbcoun` (`counno`, `counpass`, `empid`) VALUES
-('1', 'counselorpass1', '1'),
-('2', 'counselorpass2', '2'),
-('3', 'counselorpass3', '3'),
-('4', 'counselorpass4', '4'),
-('5', 'counselorpass5', '5');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbempinfo`
 --
 
@@ -88,53 +36,58 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
   PRIMARY KEY (`empid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `tbempinfo`
---
-
-INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_employeeaccount`
+-- Table structure for table `tbl_addnewproduct`
 --
 
-DROP TABLE IF EXISTS `tbl_employeeaccount`;
-CREATE TABLE IF NOT EXISTS `tbl_employeeaccount` (
-  `empid` int NOT NULL AUTO_INCREMENT,
-  `employeeCode` varchar(50) NOT NULL,
-  `employeePassword` varchar(255) NOT NULL,
-  `employeeEmail` varchar(255) NOT NULL,
-  `employeeRole` enum('Admin','Stock In','Stock Out') DEFAULT NULL,
-  `resetCode` int DEFAULT NULL,
-  PRIMARY KEY (`employeeCode`),
-  KEY `empid` (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbl_employeeaccount`
---
-
-INSERT INTO `tbl_employeeaccount` (`empid`, `employeeCode`, `employeePassword`, `employeeEmail`, `employeeRole`, `resetCode`) VALUES
-(1, '21-34994', 'admin', '21-34994@g.batstate-u.edu.ph', 'Admin', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_productinfo`
---
-
-DROP TABLE IF EXISTS `tbl_productinfo`;
-CREATE TABLE IF NOT EXISTS `tbl_productinfo` (
-  `productID` int NOT NULL AUTO_INCREMENT,
-  `productName` varchar(255) NOT NULL,
-  `productSize` varchar(50) NOT NULL,
-  `productPrice` decimal(10,2) DEFAULT '0.00',
-  `productImage` blob NOT NULL,
-  PRIMARY KEY (`productID`)
+DROP TABLE IF EXISTS `tbl_addnewproduct`;
+CREATE TABLE IF NOT EXISTS `tbl_addnewproduct` (
+  `prodid` int NOT NULL,
+  `Product_Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `quantity` int NOT NULL,
+  `Price` decimal(10,0) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_addnewproduct`
+--
+
+INSERT INTO `tbl_addnewproduct` (`prodid`, `Product_Name`, `description`, `size`, `quantity`, `Price`) VALUES
+(21211, 'wrqfas', 'fasfasfa', 'large', 141, '24141'),
+(111111, 'polol', 'bsu lipa polo', 'small', 10, '500'),
+(22222, 'polo', 'bsu', 'large', 1, '600'),
+(55555, 'pants', 'bsu pants', 'small', 2, '100'),
+(7, 'polo', 'dasd', 'XL', 47, '500'),
+(7878, 'pants', 'weqweqe', 'XXL', 8, '500');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_createemp`
+--
+
+DROP TABLE IF EXISTS `tbl_createemp`;
+CREATE TABLE IF NOT EXISTS `tbl_createemp` (
+  `empID` int NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_createemp`
+--
+
+INSERT INTO `tbl_createemp` (`empID`, `role`, `username`, `password`) VALUES
+(0, 'employee_in', 'kenenth', '$2y$10$LjtoqnY0whTPiQoPDjkbt.QCqVm/EHkhvfq5bEUJuEM5DLe2egPQO'),
+(0, 'employee_in', 'kenneth ilong', '$2y$10$3gEiPUvsbtAW/pvHGv7lO.Cy0EuF/nD5Hm118rq8TFgiDp7tDBopi'),
+(0, 'employee_out', 'kenneth mabaho', '$2y$10$ij/z/1PN/t.NHZphgrzuN.FQPXOyhqJoEsCSHep7IArOnEQnBBMnq'),
+(0, 'employee_out', 'sdadsad', '$2y$10$hYluug4VtIrNYXaU.i5HPepTDPXBPbc6WvzhvpYzDMqVyR8j5/zCq'),
+(0, 'employee_out', 'Harbiii Nikol', '$2y$10$fN8Gj4zJ2VzsAqDI.n7o1ObQy7zMSuPW8nIrsKLoTxFEewmYFFFmm');
 
 -- --------------------------------------------------------
 
@@ -144,15 +97,36 @@ CREATE TABLE IF NOT EXISTS `tbl_productinfo` (
 
 DROP TABLE IF EXISTS `tbl_stockin`;
 CREATE TABLE IF NOT EXISTS `tbl_stockin` (
-  `recordID` int NOT NULL AUTO_INCREMENT,
-  `recordDate` date NOT NULL,
-  `employeeCode` varchar(50) NOT NULL,
-  `productID` int NOT NULL,
-  `productQuantity` int NOT NULL,
-  PRIMARY KEY (`recordID`),
-  KEY `employeeCode` (`employeeCode`),
-  KEY `productID` (`productID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `numRecords` int NOT NULL AUTO_INCREMENT,
+  `employeeId` int NOT NULL,
+  `productId` int NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `quantity` int NOT NULL,
+  `quantityout` int NOT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`numRecords`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_stockin`
+--
+
+INSERT INTO `tbl_stockin` (`numRecords`, `employeeId`, `productId`, `description`, `size`, `quantity`, `quantityout`, `date`) VALUES
+(11, 1, 3, 'Blouse', 'medium', 55555, 0, '2023-11-20 16:00:00'),
+(10, 1, 2, 'Pants', 'small', 2444444, 0, '2023-11-18 16:00:00'),
+(9, 1, 1, 'Pants', 'small', 1111, 0, '2023-11-20 16:00:00'),
+(8, 2, 1, 'Polo', 'small', 6, 0, '2023-11-19 16:00:00'),
+(7, 1, 1, 'Polo', 'small', 104, 0, '2023-11-19 16:00:00'),
+(12, 1, 2, 'Polo', 'medium', 100, 0, '2023-11-19 16:00:00'),
+(13, 1, 2, 'Polo', 'medium', 100, 0, '2023-11-19 16:00:00'),
+(14, 1, 2, 'Polo', 'medium', 20, 0, '2023-11-22 16:00:00'),
+(15, 1, 21211, 'Polo', 'small', 1000, 0, '2023-11-18 16:00:00'),
+(16, 1, 21211, 'Polo', 'small', 1000, 0, '2023-11-19 16:00:00'),
+(17, 1, 22222, 'Polo', '2xl', 100, 0, '2023-11-18 16:00:00'),
+(18, 1, 7, 'Skirt', 'medium', 100, 0, '2023-11-19 16:00:00'),
+(19, 1, 7, 'Skirt', 'medium', 100, 0, '2023-11-19 16:00:00'),
+(20, 1, 7, 'Polo', 'xl', 100, 0, '2023-11-19 16:00:00');
 
 -- --------------------------------------------------------
 
@@ -162,73 +136,28 @@ CREATE TABLE IF NOT EXISTS `tbl_stockin` (
 
 DROP TABLE IF EXISTS `tbl_stockout`;
 CREATE TABLE IF NOT EXISTS `tbl_stockout` (
-  `salesID` int NOT NULL AUTO_INCREMENT,
-  `salesDate` date NOT NULL,
-  `employeeCode` varchar(50) NOT NULL,
-  `productID` int NOT NULL,
-  `productQuantity` int NOT NULL,
-  PRIMARY KEY (`salesID`),
-  KEY `employeeCode` (`employeeCode`),
-  KEY `productID` (`productID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbreports`
---
-
-DROP TABLE IF EXISTS `tbreports`;
-CREATE TABLE IF NOT EXISTS `tbreports` (
-  `reportno` int NOT NULL AUTO_INCREMENT,
-  `reporttype` varchar(255) DEFAULT NULL,
-  `report` text,
-  `studno` varchar(50) DEFAULT NULL,
-  `status` varchar(200) DEFAULT NULL,
-  `remarks` varchar(200) DEFAULT NULL,
-  `datefiled` datetime DEFAULT NULL,
-  `dateresolved` datetime DEFAULT NULL,
-  PRIMARY KEY (`reportno`),
-  KEY `studno` (`studno`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbreports`
---
-
-INSERT INTO `tbreports` (`reportno`, `reporttype`, `report`, `studno`, `status`, `remarks`, `datefiled`, `dateresolved`) VALUES
-(1, 'Bullying', 'I was bullied by a blockmate.', '21-39325', 'Pending', NULL, '2023-11-22 20:45:44', NULL),
-(2, 'Academic Appeal', 'I received a failing grade in my class, is there something I can do about it?', '21-39325', 'Pending', NULL, '2023-11-22 20:46:10', NULL),
-(3, 'Harassment & Discrimination', 'I was harassed.', '21-39325', 'Pending', NULL, '2023-11-22 20:46:44', NULL),
-(4, 'Course Availability & Scheduling', 'No professor in 1 subject.', '21-39325', 'Resolved', 'Done.', '2023-11-22 20:46:55', '2023-11-22 20:49:14'),
-(5, 'Harassment & Discrimination', 'I was harassed.', '21-39325', 'Pending', NULL, '2023-11-22 20:47:08', NULL),
-(6, 'Course Availability & Scheduling', 'No professor in 1 subject.', '21-30073', 'In Progress', 'Will look into this, thanks.', '2023-11-22 20:48:14', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbstud`
---
-
-DROP TABLE IF EXISTS `tbstud`;
-CREATE TABLE IF NOT EXISTS `tbstud` (
-  `studno` varchar(50) NOT NULL,
-  `studpass` varchar(255) NOT NULL,
-  `studid` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`studno`),
-  KEY `fk_tbstud_studid` (`studid`)
+  `numRecords` int NOT NULL DEFAULT '0',
+  `employeeId` int NOT NULL,
+  `productId` int NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `quantity` int NOT NULL,
+  `quantityout` int NOT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tbstud`
+-- Dumping data for table `tbl_stockout`
 --
 
-INSERT INTO `tbstud` (`studno`, `studpass`, `studid`) VALUES
-('21-34780', 'bonjlaygo', '2134780'),
-('21-39414', 'ivanrubion', '2139414'),
-('21-30073', 'belletrayfalgar', '2130073'),
-('21-39325', 'johnronaldlopez', '2139325'),
-('21-31054', 'sethtada', '2131054');
+INSERT INTO `tbl_stockout` (`numRecords`, `employeeId`, `productId`, `description`, `size`, `quantity`, `quantityout`, `date`) VALUES
+(11, 1, 3, 'Blouse', 'medium', 55555, 0, '2023-11-20 16:00:00'),
+(10, 1, 2, 'Pants', 'small', 2444444, 0, '2023-11-18 16:00:00'),
+(9, 1, 1, 'Pants', 'small', 1111, 0, '2023-11-20 16:00:00'),
+(8, 2, 1, 'Polo', 'small', 6, 0, '2023-11-19 16:00:00'),
+(7, 1, 1, 'Polo', 'small', 104, 0, '2023-11-19 16:00:00'),
+(0, 1, 2, 'Polo', 'medium', 100, 0, '2023-11-27 16:00:00'),
+(0, 1, 21211, 'Polo', 'small', 222, 0, '2023-11-19 16:00:00');
 
 -- --------------------------------------------------------
 
@@ -243,18 +172,7 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2139415 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbstudinfo`
---
-
-INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(2139325, 'Lopez', 'John Ronald', 'BSIT-BA-3101'),
-(2130073, 'Trayfalgar', 'Belle Colleen', 'BSIT-BA-3101'),
-(2131054, 'Tada', 'Seth Aldous', 'BSIT-BA-3101'),
-(2139414, 'Rubion', 'Ivan Gabriel', 'BSIT-BA-3101'),
-(2134780, 'Laygo', 'Bonjohn Benedict', 'BSIT-BA-3101');
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
