078156d206a5703c3f90269382f3cfd2aacfd915
Jan Patrick Fabellon <145630466+janpatrick234@users.noreply.github.com>
2023-11-30 20:38:21 +0800

    Add files via upload

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 30, 2023 at 08:38 AM
+-- Generation Time: Nov 30, 2023 at 12:36 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_ba3101a`
+-- Database: `db_ba3101`
 --
 
 -- --------------------------------------------------------
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `tbadmin` (
   `empid` int NOT NULL,
   PRIMARY KEY (`admin_ID`),
   KEY `empid` (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -53,17 +53,7 @@ CREATE TABLE IF NOT EXISTS `tbborrow` (
   PRIMARY KEY (`borrow_ID`),
   KEY `studid` (`studid`),
   KEY `equipment_ID` (`equipment_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbborrow`
---
-
-INSERT INTO `tbborrow` (`borrow_ID`, `studid`, `equipment_quantity`, `equipment_ID`, `date_borrowed`) VALUES
-(1, 7, 2, 4, '2023-11-30'),
-(2, 7, 3, 5, '2023-11-30'),
-(3, 7, 1, 4, '2023-11-30'),
-(4, 7, 5, 4, '2023-11-30');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -78,19 +68,14 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbempinfo`
 --
 
 INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics'),
-(2, 'Cruz', 'Mark', 'cics'),
-(3, 'Miranda', 'Rocky', 'cics'),
-(4, 'Peloramas', 'Angela', 'cics'),
-(5, 'Dela Pena', 'Leo', 'cics'),
-(6, 'Macalla', 'Dorothy', 'cics');
+(1, 'aguila', 'nina', 'cics');
 
 -- --------------------------------------------------------
 
@@ -106,37 +91,8 @@ CREATE TABLE IF NOT EXISTS `tbequipment` (
   `equipment_description` varchar(200) NOT NULL,
   `equip_photos` blob NOT NULL,
   `is_active` tinyint(1) DEFAULT '1',
-  `date_arrived` date DEFAULT NULL,
+  `date_arrived` date NOT NULL,
   PRIMARY KEY (`equipment_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbequipment`
---
-
-INSERT INTO `tbequipment` (`equipment_ID`, `equipment_name`, `equipment_quantity`, `equipment_description`, `equip_photos`, `is_active`, `date_arrived`) VALUES
-(4, 'Soccer', 2, 'qwefds', 0x2e2e2f696d616765732f696d6167652d6571702f62616c6c5f322e6a666966, 1, '2023-11-30'),
-(6, 'Evgfuyv', 2, 'fewgwrgrgre', 0x2e2e2f696d616765732f696d6167652d6571702f7177712e6a7067, 1, '2023-11-30'),
-(5, 'Futsal', 7, 'asiug', 0x2e2e2f696d616765732f696d6167652d6571702f62616c6c2e6a7067, 1, '2023-11-30');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbreturned`
---
-
-DROP TABLE IF EXISTS `tbreturned`;
-CREATE TABLE IF NOT EXISTS `tbreturned` (
-  `return_ID` int NOT NULL AUTO_INCREMENT,
-  `borrow_ID` int NOT NULL,
-  `returned_date` date NOT NULL,
-  `studid` int NOT NULL,
-  `returned_quantity` int NOT NULL,
-  `equipment_damaged` int NOT NULL,
-  `equipment_missing` int NOT NULL,
-  PRIMARY KEY (`return_ID`),
-  KEY `borrow_ID` (`borrow_ID`),
-  KEY `studid` (`studid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -154,20 +110,7 @@ CREATE TABLE IF NOT EXISTS `tbstaff` (
   `empid` int NOT NULL,
   PRIMARY KEY (`staff_ID`),
   KEY `empid` (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbstaff`
---
-
-INSERT INTO `tbstaff` (`staff_ID`, `staff_user`, `staff_pass`, `staff_position`, `empid`) VALUES
-(1, 'Marky', 'poiuy', 'OUT', 2),
-(2, 'gelapeloramas', '1234mnb', 'IN', 4),
-(3, 'gelapeloramas', '1234mnb', 'IN', 4),
-(4, 'rockyy', '1234mnb', 'OUT', 3),
-(5, 'dorodogs', 'top', 'IN', 6),
-(6, 'ftiytc', '98gyg', 'OUT', 1),
-(7, 'dthrth', 'rtsjsrt', 'OUT', 6);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -182,14 +125,7 @@ CREATE TABLE IF NOT EXISTS `tbstudent` (
   `studid` int NOT NULL,
   PRIMARY KEY (`sr_ID`),
   KEY `studid` (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbstudent`
---
-
-INSERT INTO `tbstudent` (`sr_ID`, `sr_code`, `studid`) VALUES
-(1, '21-32471', 7);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
