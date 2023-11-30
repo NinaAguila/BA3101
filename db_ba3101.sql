b55faac386b964a1e0a7f4100e83875e314b4002
John Ronald Lopez <21-39325@g.batstate-u.edu.ph>
2023-11-30 10:59:08 +0800

    add files via upload

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -1,10 +1,9 @@
 -- phpMyAdmin SQL Dump
-
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 23, 2023 at 02:35 AM
+-- Generation Time: Nov 30, 2023 at 02:57 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -21,6 +20,100 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_ba3101`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `adminID` int NOT NULL AUTO_INCREMENT,
+  `empid` int DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`adminID`),
+  UNIQUE KEY `email` (`email`),
+  KEY `empid` (`empid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applicant`
+--
+
+DROP TABLE IF EXISTS `applicant`;
+CREATE TABLE IF NOT EXISTS `applicant` (
+  `applicantID` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`applicantID`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application`
+--
+
+DROP TABLE IF EXISTS `application`;
+CREATE TABLE IF NOT EXISTS `application` (
+  `applicationID` int NOT NULL AUTO_INCREMENT,
+  `jobID` int NOT NULL,
+  `applicantID` int NOT NULL,
+  `hrID` int NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `contact` varchar(20) DEFAULT NULL,
+  `resume` varchar(255) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`applicationID`),
+  KEY `jobID` (`jobID`),
+  KEY `applicantID` (`applicantID`),
+  KEY `hrID` (`hrID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hr`
+--
+
+DROP TABLE IF EXISTS `hr`;
+CREATE TABLE IF NOT EXISTS `hr` (
+  `HRID` int NOT NULL AUTO_INCREMENT,
+  `empid` int DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`HRID`),
+  UNIQUE KEY `email` (`email`),
+  KEY `empid` (`empid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobpost`
+--
+
+DROP TABLE IF EXISTS `jobpost`;
+CREATE TABLE IF NOT EXISTS `jobpost` (
+  `jobID` int NOT NULL AUTO_INCREMENT,
+  `adminID` int NOT NULL,
+  `jobType` varchar(100) DEFAULT NULL,
+  `department` varchar(100) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `requirement` varchar(255) DEFAULT NULL,
+  `salary` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`jobID`),
+  KEY `adminID` (`adminID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -77,6 +170,27 @@ INSERT INTO `tbcoun` (`counno`, `counpass`, `empid`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbempaccount`
+--
+
+DROP TABLE IF EXISTS `tbempaccount`;
+CREATE TABLE IF NOT EXISTS `tbempaccount` (
+  `empaccountId` int NOT NULL AUTO_INCREMENT,
+  `empid` int NOT NULL,
+  `department_id` int DEFAULT NULL,
+  `emp_email` varchar(255) NOT NULL,
+  `emp_profile` longblob,
+  `emp_password` varchar(255) NOT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`empaccountId`),
+  KEY `fk_empinfo_changes` (`empid`),
+  KEY `fk_department_emp` (`department_id`),
+  KEY `fk_emp_roles` (`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbempinfo`
 --
 
@@ -94,7 +208,25 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
 --
 
 INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'Cerezo', 'Ezekiel Eisen', 'CICS');
+(1, 'aguila', 'nina', 'cics');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_account`
+--
+
+DROP TABLE IF EXISTS `tbl_account`;
+CREATE TABLE IF NOT EXISTS `tbl_account` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userImg` longblob NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  `userDept` varchar(255) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  `userPass` varchar(255) NOT NULL,
+  `userType` varchar(255) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -124,6 +256,24 @@ INSERT INTO `tbl_employeeaccount` (`employeeCount`, `employeeID`, `employeePassw
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_office`
+--
+
+DROP TABLE IF EXISTS `tbl_office`;
+CREATE TABLE IF NOT EXISTS `tbl_office` (
+  `officeAccID` int NOT NULL AUTO_INCREMENT,
+  `officeImg` longblob NOT NULL,
+  `designation` varchar(255) NOT NULL,
+  `officeEmail` varchar(255) NOT NULL,
+  `officePass` varchar(255) NOT NULL,
+  `employeeID` int NOT NULL,
+  PRIMARY KEY (`officeAccID`),
+  KEY `empID(FK)` (`employeeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_products`
 --
 
@@ -134,6 +284,45 @@ CREATE TABLE IF NOT EXISTS `tbl_products` (
   `productImage` blob NOT NULL,
   PRIMARY KEY (`productID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_reqhistory`
+--
+
+DROP TABLE IF EXISTS `tbl_reqhistory`;
+CREATE TABLE IF NOT EXISTS `tbl_reqhistory` (
+  `histID` int NOT NULL AUTO_INCREMENT,
+  `reqStatus` varchar(255) NOT NULL,
+  `statusDate` date NOT NULL,
+  `orgID` int NOT NULL,
+  `reqID` int NOT NULL,
+  `officeID` int NOT NULL,
+  PRIMARY KEY (`histID`),
+  KEY `userID_FK(tbl_reqH)` (`orgID`),
+  KEY `reqID_FK(tbl_reqH)` (`reqID`),
+  KEY `officeID(tbl_reqH)` (`officeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_requests`
+--
+
+DROP TABLE IF EXISTS `tbl_requests`;
+CREATE TABLE IF NOT EXISTS `tbl_requests` (
+  `reqID` int NOT NULL AUTO_INCREMENT,
+  `reqEventName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `reqLetter` longblob NOT NULL,
+  `reqEventDate` date NOT NULL,
+  `reqDeadline` date NOT NULL,
+  `userID` int NOT NULL,
+  `currentOffice` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Program Chair',
+  PRIMARY KEY (`reqID`),
+  KEY `userID(tbl_req)` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -259,6 +448,25 @@ INSERT INTO `tbstud` (`studno`, `studpass`, `studid`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbstudentaccount`
+--
+
+DROP TABLE IF EXISTS `tbstudentaccount`;
+CREATE TABLE IF NOT EXISTS `tbstudentaccount` (
+  `studaccountid` int NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL,
+  `department_id` int DEFAULT NULL,
+  `student_email` varchar(255) NOT NULL,
+  `student_profile` longblob,
+  `student_password` varchar(255) NOT NULL,
+  PRIMARY KEY (`studaccountid`),
+  KEY `fk_department_student` (`department_id`),
+  KEY `fk_studinfo_changes` (`studid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbstudinfo`
 --
 
@@ -269,20 +477,143 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2139415 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbstudinfo`
 --
 
 INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(2139325, 'Lopez', 'John Ronald', 'BSIT-BA-3101'),
-(2130073, 'Trayfalgar', 'Belle Colleen', 'BSIT-BA-3101'),
-(2131054, 'Tada', 'Seth Aldous', 'BSIT-BA-3101'),
-(2139414, 'Rubion', 'Ivan Gabriel', 'BSIT-BA-3101'),
-(2134780, 'Laygo', 'Bonjohn Benedict', 'BSIT-BA-3101');
-=======
+(1, 'parker', 'peter', 'bsit'),
+(2, 'kent', 'clark', 'bscs');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_attendees`
+--
+
+DROP TABLE IF EXISTS `tb_attendees`;
+CREATE TABLE IF NOT EXISTS `tb_attendees` (
+  `attendee_id` int NOT NULL AUTO_INCREMENT,
+  `event_id` int NOT NULL,
+  `student_id` int DEFAULT NULL,
+  `empid` int DEFAULT NULL,
+  PRIMARY KEY (`attendee_id`),
+  KEY `event_id` (`event_id`),
+  KEY `student_id` (`student_id`),
+  KEY `empid` (`empid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_department`
+--
+
+DROP TABLE IF EXISTS `tb_department`;
+CREATE TABLE IF NOT EXISTS `tb_department` (
+  `department_id` int NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`department_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_department`
+--
+
+INSERT INTO `tb_department` (`department_id`, `department_name`) VALUES
+(1, 'CICS'),
+(2, 'CABE'),
+(3, 'CAS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_event`
+--
+
+DROP TABLE IF EXISTS `tb_event`;
+CREATE TABLE IF NOT EXISTS `tb_event` (
+  `event_id` int NOT NULL AUTO_INCREMENT,
+  `event_title` varchar(255) NOT NULL,
+  `event_detail` text NOT NULL,
+  `event_date` date NOT NULL,
+  `header_image` longblob,
+  `department_id` int DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`event_id`),
+  KEY `department_id` (`department_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_event_images`
+--
+
+DROP TABLE IF EXISTS `tb_event_images`;
+CREATE TABLE IF NOT EXISTS `tb_event_images` (
+  `image_id` int NOT NULL AUTO_INCREMENT,
+  `event_id` int DEFAULT NULL,
+  `image_filename` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`image_id`),
+  KEY `event_id` (`event_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_roles`
+--
+
+DROP TABLE IF EXISTS `tb_roles`;
+CREATE TABLE IF NOT EXISTS `tb_roles` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `unique_role_name` (`role_name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_roles`
+--
+
+INSERT INTO `tb_roles` (`role_id`, `role_name`) VALUES
+(1, 'Admin'),
+(2, 'Teacher');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rso`
+--
+
+DROP TABLE IF EXISTS `tb_rso`;
+CREATE TABLE IF NOT EXISTS `tb_rso` (
+  `rso_id` int NOT NULL AUTO_INCREMENT,
+  `rso_name` varchar(50) NOT NULL,
+  `rso_password` varchar(20) NOT NULL,
+  `department_id` int DEFAULT NULL,
+  `rso_email` varchar(50) NOT NULL,
+  `rso_profile` longblob,
+  PRIMARY KEY (`rso_id`),
+  UNIQUE KEY `unique_rso_email` (`rso_email`),
+  KEY `department_id` (`department_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DELIMITER $$
+--
+-- Events
+--
+DROP EVENT IF EXISTS `update_event_status`$$
+CREATE DEFINER=`root`@`localhost` EVENT `update_event_status` ON SCHEDULE EVERY 1 SECOND STARTS '2023-11-20 12:35:58' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+    UPDATE `tb_event` SET `status` = 'upcoming' WHERE `event_date` > CURDATE();
+    UPDATE `tb_event` SET `status` = 'ongoing' WHERE `event_date` = CURDATE();
+    UPDATE `tb_event` SET `status` = 'ended' WHERE `event_date` < CURDATE();
+END$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
