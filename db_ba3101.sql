223121688ae169b0169d491bcedd4da19c88d798
Kyla Claire Pusag <143685825+KylaClairePusag@users.noreply.github.com>
2023-11-30 11:38:19 +0800

    Update db_ba3101.sql

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
-  PRIMARY KEY (`empid`) 
+  PRIMARY KEY (`empid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -124,7 +124,7 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 --
